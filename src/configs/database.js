e06acea950d578c5fa6e0fbe084d2eb359113fe8
Priscila <pry_rocha@outlook.com>
2021-07-07 23:31:19 -0300

    Atualiza data

--- src/configs/database.js
+++ src/configs/database.js
@@ -5,7 +5,9 @@ const connect = async () => {
     try {
         mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
         })
         console.log('db connected')
     } catch (error) {
